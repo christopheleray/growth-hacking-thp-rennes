ae06cb519dae497646fd4a17b5a8debeb189b2b2
christopheleray <christophe.leray1@gmail.com>
2018-08-15 16:14:41 +0200

    maile with gmail et maijet ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+   # application.rb or config/environments specific settings, which take precedence
+    config.action_mailer.delivery_method = :mailjet_api
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
