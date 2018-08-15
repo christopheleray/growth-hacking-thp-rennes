5b76a001ae1ef732ae57499f2e81bbc2ba7f075c
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-15 16:11:39 +0200

    scrap parlement ok

--- app/services/scrap_parlement.rb
+++ app/services/scrap_parlement.rb
@@ -23,21 +23,33 @@ class ScrapParlement
   def deputes_infos_list
     p '-' * 20 + 'starting the search for all the infos' + '-' * 20
     @depute_list.each do |url|
-
       url = "https://www.nosdeputes.fr#{url}"
       doc = Nokogiri::HTML(open("#{url}"))
       p '-' * 20 + 'Nokogiri opened the page correctly' + '-' * 20
-      names = doc.xpath('//h1').text
-      emails = doc.xpath('/html/body/div[1]/div[5]/div/div[2]/div[1]/ul[2]/li[1]/ul/li[1]/a').text
-      @mailing_list << hash
+      p doc.xpath('//h1').text
+      p doc.xpath('/html/body/div[1]/div[5]/div/div[2]/div[1]/ul[2]/li[1]/ul/li[1]/a').text
+      @mailing_list["#{doc.xpath('//h1').text}"] = doc.xpath('/html/body/div[1]/div[5]/div/div[2]/div[1]/ul[2]/li[1]/ul/li[1]/a').text
     end
+    p @mailing_list
+    p '-' * 20 + 'Scrapping of the mailing list : done' + '-' * 20
   end
 
   def save
-
+    p '-' * 20 + 'wiping de la table deputes' + '-' * 20
+    Depute.destroy_all
+    p '-' * 20 + 'stockage de la mailing list dans la db' + '-' * 20
+    @mailing_list.each do |key, value|
+      p '-' * 20 + 'passage dans le hash' + '-' * 20
+      mailing_save = Depute.create(name: key, email: value)
+      p mailing_save
+    end
+    p '-' * 20 + 'stockage de la mailig list réussis' + '-' * 20
   end
 
-  def perform
-    get_depute_list
+  def self.perform
+    actions = ScrapParlement.new
+    actions.get_depute_list
+    actions.deputes_infos_list
+    actions.save
   end
 end
