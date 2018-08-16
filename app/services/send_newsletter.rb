25c0961f8916fc71db73e026c159f26242c29c6c
christopheleray <christophe.leray1@gmail.com>
2018-08-16 17:37:32 +0200

    newsletter mailchimp

--- app/services/send_newsletter.rb
+++ app/services/send_newsletter.rb
@@ -2,24 +2,31 @@ require 'uri'
 require 'json'
 require 'net/http'
 require 'dotenv'
+require 'mailchimp'
+
 Dotenv.load
 
 class SendNewsletter
-  attr_accessor :subscriber
+  attr_accessor :mailchimp
 
   def initialize
-
+    @mailchimp = Mailchimp::API.new('6576586af43be783fc042c3dcb544424-us19')
   end
 
-  def update_subscriber_list
-    mailchimp_api_key =  ENV['MAILCHIMP_API_KEY']
-    url  = URI("")
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(url)
-    response = http.request(request)
-    @body = JSON.parse(response.read_body)
-    MovieResult.delete_all
+  def get_member_list
+    puts "get member list"
+    @json = mailchimp.lists.members('7b033f50e2')[:'data']
+    puts @json 
+    puts "fin get member list"
+    @json.each do 
+      puts @json['email']['email']
+   #   Subscriber.create(email: @json['email'][])
+    end
+  end
+
+  def update_db_subscriber(json)
+
+
   end
 
   def send_newsletter
@@ -29,8 +36,9 @@ class SendNewsletter
   end
 
   def perform
-    update_subscriber_list
-    send_newsletter
+    get_member_list
+    #update_subscriber_list
+    #send_newsletter
   end
 
 end
